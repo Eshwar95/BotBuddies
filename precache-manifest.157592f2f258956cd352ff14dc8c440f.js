c6d07c67b82be292178217308d95089efcc3dffe
Eshwar Vasudevan <eshwar@Eshwars-MacBook-Pro.local>
2020-06-26 00:03:59 -0400

    Updates

---
+++ precache-manifest.157592f2f258956cd352ff14dc8c440f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b0ef55dd5321b8a408d00c29da9090",
+    "url": "/botbuddies/index.html"
+  },
+  {
+    "revision": "1697deed56942b8d91dc",
+    "url": "/botbuddies/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f66dc429f2c1eb9602a0",
+    "url": "/botbuddies/static/css/main.ec2fd461.chunk.css"
+  },
+  {
+    "revision": "1697deed56942b8d91dc",
+    "url": "/botbuddies/static/js/2.88cccb87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/botbuddies/static/js/2.88cccb87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66dc429f2c1eb9602a0",
+    "url": "/botbuddies/static/js/main.1da2a6ec.chunk.js"
+  },
+  {
+    "revision": "0aedba9d40bbe666f6ac",
+    "url": "/botbuddies/static/js/runtime-main.821030f9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/botbuddies/static/media/SEGA.12a3db8b.woff"
+  }
+]);
